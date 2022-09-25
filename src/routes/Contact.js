b2d7c935f2329871f82f4e762f5fdca89c2e383a
HuiwenShi <huiwenshi@outlook.com>
2022-09-24 21:31:48 -0400

    add social

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -5,7 +5,14 @@ function Contact() {
     <div className='content_bottom contact' id='contact'>
       <p>Thanks for stopping by~</p>
       <p>
-        👋🏼 Let's <a href='https://calendly.com/its-hshi/30min'>Chat</a>
+        👋🏼 Let's{' '}
+        <a
+          href='https://calendly.com/its-hshi/30min'
+          target='_blank'
+          rel='noreferrer'
+        >
+          Chat
+        </a>
       </p>
     </div>
   );
