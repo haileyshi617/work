faf234b4d37061ee1f2aadab0a99848e8c2ec470
HuiwenShi <huiwenshi@outlook.com>
2022-09-24 21:29:59 -0400

    add social

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -9,17 +9,32 @@ For the past 7 years I’ve been working on creating products that promotes conn
         <br />
         <div className='socials-container'>
           <div className='animated zoomIn'>
-            <a className='social'>
+            <a
+              className='social'
+              target='_blank'
+              rel='noreferrer'
+              href='https://www.linkedin.com/in/huiwen-shi-a0815b1a2/'
+            >
               <i className='fab fa-linkedin'></i>
             </a>
           </div>
           <div className='animated zoomIn'>
-            <a className='social'>
+            <a
+              className='social'
+              target='_blank'
+              rel='noreferrer'
+              href='https://www.instagram.com/hshi123/'
+            >
               <i className='fab fa-instagram'></i>
             </a>
           </div>
           <div className='animated zoomIn'>
-            <a className='social'>
+            <a
+              className='social'
+              target='_blank'
+              rel='noreferrer'
+              href='https://dribbble.com/gaandng'
+            >
               <i className='fab fa-dribbble'></i>
             </a>
           </div>
